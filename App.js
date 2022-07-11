bea6f4fdfd6cc4aa2799cb05cc42370ce9320314
Pemo11235 <matteoluceri@msn.com>
2022-07-11 08:53:39 +0200

    feat(RepositoryList/Item): create list and item for repository

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Ciao !</Text>
-      <StatusBar style='auto' />
-    </View>
-  )
+import Main from './src/components/Main'
+const App = () => {
+  return <Main />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,29 @@
+import { Text, View } from 'react-native'
+
+const RepositoryItem = ({
+  repository: {
+    id,
+    fullName,
+    description,
+    language,
+    forksCount,
+    stargazersCount,
+    ratingAverage,
+    reviewCount,
+    ownerAvatarUrl,
+  },
+}) => {
+  return (
+    <View>
+      <Text>Full Name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Rating: {ratingAverage}</Text>
+    </View>
+  )
+}
+
+export default RepositoryItem
