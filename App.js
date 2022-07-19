8dd0ab9cbb544bbc7d38b2e51914d480ce042e94
Pemo11235 <matteoluceri@msn.com>
2022-07-19 17:27:40 +0200

    feat(app): add dotenv and ENV var

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { ApolloProvider } from '@apollo/client'
 import { NativeRouter } from 'react-router-native'
 import Main from './src/components/Main'
 import createApolloClient from './src/utils/apolloClient'
+import Constants from 'expo-constants'
 
 const apolloClient = createApolloClient()
 const App = () => {
+  console.log(Constants.manifest.extra.env)
   return (
     <NativeRouter>
       <ApolloProvider client={apolloClient}>
